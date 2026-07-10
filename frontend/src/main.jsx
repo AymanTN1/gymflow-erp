84740200bc23533575637de9aa667732597f1cd9
ayman <aymantantani18@gmail.com>
2026-07-10 13:40:45 +0100

    feat(pwa): transformation en application mobile pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Une nouvelle version est disponible. Voulez-vous recharger l'application ?")) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline")
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
